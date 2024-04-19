b30de12f55f48d5f75ab50b533b09e6c88c482ea
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-04-19 20:17:44 +0900

    20240419

--- donghwa/js/common.js
+++ donghwa/js/common.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    $('footer .f_site .open').on('click', function(){
-        $('footer .f_site').addClass('on')
-    })
-    $('footer .f_site .close').on('click', function(){
-        $('footer .f_site').removeClass('on')
-    })
+
 })//$(document).ready
--- donghwa/js/main.js
+++ donghwa/js/main.js
@@ -1,62 +1,42 @@
 $(document).ready(function(){
-    var sticky = new Sticky('[data-sticky]');  
-    
-    var scrolling //스크롤된 값
-    var window_h // 윈도우 높이
-    
-    function scroll_chk(){
-        scrolling = $(window).scrollTop();
-        window_h = $(window).height();
-        opacity_change('.intro .intro_tit', 0, 0.002, 'hide')
-        opacity_change('.intro .bg', 450, 0.002, 'show')
-        move_change('.intro .photo ul li.move', 0.05)
+    const swiper = new Swiper('.swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-        if((scrolling >= $('.visual').offset().top) && (scrolling <= $('footer').offset().top + $('footer').height())){
-            $('header').addClass('fixed')
+        // autoplay: { 
+        //     delay: 5000,
+        //     disableOnInteraction: true,
+        // },
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        slidesPerView: 3,
+	    spaceBetween: 0,
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.product .product_list .next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.product .product_list .prev',  
+        },
+
+    });
+
+    let scrolling 
+    function scroll_chk(){
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){
+            $('header').addClass('fiexd')
         }else{
-            $('header').removeClass('fixed')
-        }
-    }
-    function opacity_change(obj, start, speed, mode){
-        if(mode == 'hide'){
-            if(scrolling > start){
-                //console.log(scrolling, start, speed)
-                $(obj).css({"opacity": (1-((scrolling - start)*speed))})
-            }else{
-                $(obj).css({"opacity": 1});    
-            }
-        }else{
-            if(scrolling > start){
-                $(obj).css({"opacity": (scrolling - start)*speed})
-            }else{
-                $(obj).css({"opacity": 0});    
-            }
-        }
-    }
-    function move_change(obj, speed){
-        console.log($(obj).offset().top)
-        if(scrolling >= ($(obj).offset().top - window_h)){
-            $(obj).attr('style', 'transform:translate(0, '+ ($(obj).offset().top - scrolling - window_h)*speed  +'px)')
+            $('header').removeClass('fiexd')
         }
     }
     scroll_chk()
-    $(window).resize(function(){
-        scroll_chk()
-    })
     $(window).scroll(function(){
         scroll_chk()
     })
-  
-    /* guide1 */
-    
-	$('.guide1 .list ul li').on('mouseenter', function(){
-        $('.guide1 .list ul li').removeClass('on')
-        $(this).addClass('on')
-    })
-    
-    AOS.init({
-        offset: 130, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
-        duration: 500, // 애니메이션 효과가 작동되는 시간
-        easing: 'ease', // 가속도
-    });
 }) ////document.ready
