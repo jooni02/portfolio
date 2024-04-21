585dd73b2dfcbe325d1987f5d7f9c2f03a2997e0
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-04-22 02:39:30 +0900

    20240422

--- portfolio/js/contents.js
+++ portfolio/js/contents.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
 	$('header .gnb .inner ul li').on('mouseenter', function(){
         $('header .gnb .inner ul li').removeClass('on')
         $(this).addClass('on')
+        $('header .gnb .inner ul li[data-on2]').addClass('on')
     })
     $('header .gnb .inner').on('mouseleave', function(){
         $('header .gnb .inner ul li').removeClass('on')
         $('header .gnb .inner ul li[data-on]').addClass('on')
+        $('header .gnb .inner ul li[data-on2]').addClass('on')
     })
 
     /* top버튼 클릭하면 위로 올라가라는 의미*/
@@ -14,4 +16,9 @@ $(document).ready(function(){
             scrollTop : 0
         }, 500)
     })
+    AOS.init({
+        offset: 200, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 500, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
+    });
 })
