9110adee0341c26c20d3c432f00e2453f6001749
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-04-25 13:24:24 +0900

    20240525

--- starteacher/js/main.js
+++ starteacher/js/main.js
@@ -48,10 +48,17 @@ $(document).ready(function(){
             spaceBetween: 28,
         },
         },
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
         navigation: {
 			nextEl: '.next',
 			prevEl: '.prev',
 		},
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+	},
     
     });
 }) ////document.ready
