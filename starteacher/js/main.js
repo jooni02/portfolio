1960effd7dd65c533096bbff0ee78b7f9ae504f9
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-05-07 16:59:54 +0900

    .

--- starteacher/js/main.js
+++ starteacher/js/main.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    const teacher_swiper = new Swiper('.teacher .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const teacher_swiper = new Swiper('.teacher .swiper', {
     
-        slidesPerView: "auto", /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 18, /* 팝업과 팝업 사이 여백 */
+        slidesPerView: "auto",
+        spaceBetween: 18,
         breakpoints: {
-		768: {    /* 768px 이상일때 적용 */
+		768: {
 			slidesPerView: 4,
 			spaceBetween: 16,
 		},
-		1024: {   /* 1024px 이상일때 적용 */
+		1024: {
 			slidesPerView: 5,
 			spaceBetween: 22,
 		},
-		1280: {    /* 1280px 이상일때 적용 */
+		1280: {
 			slidesPerView: 5,
 			spaceBetween: 24,
 		},
 	    },
 
-        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        centeredSlides: true,
 
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        loop: true,
 
         speed: 5000,
         autoplay: {
@@ -30,35 +30,31 @@ $(document).ready(function(){
         
     });
 
-    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const review_swiper = new Swiper('.review .swiper', {
     
-        slidesPerView: "auto", /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        slidesPerView: "auto",
+        spaceBetween: 16,
         breakpoints: {
-        768: {    /* 768px 이상일때 적용 */
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {   /* 1024px 이상일때 적용 */
+        325: { 
             slidesPerView: 3,
-            spaceBetween: 24,
-        },
-        1280: {    /* 1280px 이상일때 적용 */
+            spaceBetween: 20,
+        },  
+        1028: {
             slidesPerView: 4,
             spaceBetween: 28,
         },
         },
 
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        loop: true,
 
         navigation: {
 			nextEl: '.next',
 			prevEl: '.prev',
 		},
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        pagination: {
+		el: '.swiper-pagination',
+		clickable: true, 
 	},
     
     });
-}) ////document.ready
+}) //document.ready
